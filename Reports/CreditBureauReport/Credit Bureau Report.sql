d0e0a88f68d93591971a81a30fefc5dd2bd06276
PoojaConflux <pooja@confluxtechnologies.com>
2014-10-30 09:49:42 +0530

    Adding Stretchy report data

--- Reports/CreditBureauReport/Credit Bureau Report.sql
+++ Reports/CreditBureauReport/Credit Bureau Report.sql
@@ -1,5 +1,6 @@
 
 
+INSERT INTO `stretchy_report` ( `report_name`, `report_type`, `report_subtype`, `report_category`, `report_sql`, `description`, `core_report`, `use_report`) VALUES ('Credit Bureau Report', 'Pentaho', NULL, 'Loan', NULL, NULL, 0, 1);
 
 INSERT INTO `stretchy_report_parameter` ( `report_id`, `parameter_id`, `report_parameter_name`) VALUES ((select id from stretchy_report where report_name = 'Credit Bureau Report'),(select id from stretchy_parameter where parameter_name='startDateSelect'), 'fromDate' );
 
