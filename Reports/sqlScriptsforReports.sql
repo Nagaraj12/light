9a94cf3036292f857744e549d069de3d005e29bc
Nagaraj Reddy <nagaraj@confluxtechnologies.com>
2014-12-31 10:23:36 +0530

    Modified reports

--- Reports/sqlScriptsforReports.sql
+++ Reports/sqlScriptsforReports.sql
@@ -89,3 +89,5 @@ INSERT INTO `stretchy_report` (`report_name`, `report_type`, `report_subtype`, `
 INSERT INTO `stretchy_report_parameter` ( `report_id`, `parameter_id`, `report_parameter_name`) VALUES ((select id from stretchy_report where report_name = 'MembersPendingForCreditApproval'),(select id from stretchy_parameter where parameter_name='OfficeIdSelectOne'), 'branch');
 
 
+INSERT INTO `stretchy_report` (`report_name`, `report_type`, `report_subtype`, `report_category`, `report_sql`, `description`, `core_report`, `use_report`) VALUES ( 'loanspendingapproval', 'Pentaho', NULL, NULL, NULL, 'loanspendingapproval', 0, 1);
+INSERT INTO `stretchy_report_parameter` ( `report_id`, `parameter_id`, `report_parameter_name`) VALUES ((select id from stretchy_report where report_name = 'loanspendingapproval'),(select id from stretchy_parameter where parameter_name='OfficeIdSelectOne'), 'branch');
